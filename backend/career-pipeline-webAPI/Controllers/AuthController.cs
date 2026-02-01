c3df0b4143d9dafbfe577fc91c1a9a7bb0f594d6
Prajakta Charde <you@example.com>
2026-02-01 22:13:13 +0000

    initialize API project structure

---
+++ backend/career-pipeline-webAPI/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace career_pipeline_webAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
